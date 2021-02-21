d42b0a2c77d66faaeeee8a32352673bbcba4c274
AyseCaTo <71521004+AyseCaTo@users.noreply.github.com>
2021-02-21 10:59:38 -0500

    User Story #2 is completed

--- src/test/java/com/libraryCT/pages/HomePage.java
+++ src/test/java/com/libraryCT/pages/HomePage.java
@@ -13,5 +13,16 @@ public class HomePage extends BasePage {
     @FindBy(xpath = "//span[.='Books']")
     public WebElement booksTitle;
 
+    @FindBy(xpath = "//a[@class='nav-link dropdown-toggle']")
+    public WebElement accountHolderName;
+
+    @FindBy(xpath = "//a[@class='dropdown-item']")
+    public WebElement logout;
+
+    public void logOut(){
+        accountHolderName.click();
+        logout.click();
+    }
+
 
 }
---
+++ src/test/java/com/libraryCT/step_definitions/Logout_Definitions.java
@@ -0,0 +1,22 @@
+package com.libraryCT.step_definitions;
+
+import com.libraryCT.pages.HomePage;
+import com.libraryCT.pages.LoginPage;
+import com.libraryCT.utilities.BrowserUtils;
+import com.libraryCT.utilities.ConfigurationReader;
+import com.libraryCT.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class Logout_Definitions {
+
+    HomePage homePage = new HomePage();
+
+    @Then("the user logout")
+    public void the_user_logout() {
+        homePage.logOut();
+    }
+
+}
